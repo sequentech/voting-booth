ef65160bd4009c3c20e1d23c698f48fb725ac1cf
Findeton <felrobelv@gmail.com>
2016-06-27 20:18:25 +0200

    add custom html variable

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,6 +81,9 @@ module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    variables: {
+      booth_html_body_include: ''
+    },
     connect: {
       main: {
         options: {
@@ -200,6 +203,7 @@ module.exports = function (grunt) {
         options: {
           remove: ['script[data-remove!="false"]','link[data-remove!="false"]'],
           append: [
+            {selector:'body',html:'<%= variables.booth_html_body_include %>'},
             {selector:'body',html:'<!--[if lte IE 8]><script src="/booth/libcompat-v3.0.1.min.js"></script><![endif]--><!--[if gte IE 9]><script src="/booth/libnocompat-v3.0.1.min.js"></script><![endif]--><!--[if !IE]><!--><script src="/booth/libnocompat-v3.0.1.min.js"></script><!--<![endif]-->'},
             {selector:'body',html:'<!--All the source code of this program under copyright. Take a look at the license details at https://github.com/agoravoting/agora-core-view/blob/master/README.md -->'},
             {selector:'body',html:'<script src="/booth/lib-v3.0.1.min.js"></script>'},
