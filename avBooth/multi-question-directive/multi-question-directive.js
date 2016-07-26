47c205de005940d727dac9f12f31b70a10402269
Eduardo Robles Elvira <edulix@agoravoting.com>
2016-07-26 13:03:13 +0200

    fix build

--- avBooth/multi-question-directive/multi-question-directive.js
+++ avBooth/multi-question-directive/multi-question-directive.js
@@ -185,10 +185,10 @@ angular.module('avBooth')
       }
 
       // check if there is a default list of options that need to be selected
-      if (!question.selectedDefaultSet
-        && question.extra_options.default_selected_option_ids
-        && angular.isArray(question.extra_options.default_selected_option_ids)
-        && question.extra_options.default_selected_option_ids.length > 0)
+      if (!question.selectedDefaultSet &&
+        question.extra_options.default_selected_option_ids &&
+        angular.isArray(question.extra_options.default_selected_option_ids) &&
+        question.extra_options.default_selected_option_ids.length > 0)
       {
         _.each(
           question.answers,
