9664c796285351bf10c264dfadfae3680c3020e8
Eduardo Robles Elvira <edulix@agoravoting.com>
2016-09-29 09:17:42 +0200

    use camel case 2..

--- avBooth/selected-options-directive/selected-options-directive.js
+++ avBooth/selected-options-directive/selected-options-directive.js
@@ -249,7 +249,7 @@ angular.module('avBooth')
         max: '=',
         min: '=',
         options: '=',
-        tally_type: '=',
+        tallyType: '=',
         presetSelectedSize: '=',
         layout: '=',
         sorted: "=",
