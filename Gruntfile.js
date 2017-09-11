6b155c380e3c7d4cee3992e81914707c59f8186b
Findeton <felix@nvotes.com>
2017-09-11 13:13:42 +0200

    add check_plugins_config task to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -352,7 +352,27 @@ module.exports = function (grunt) {
 
   });
 
-  grunt.registerTask('build',['check_config', 'jshint','clean:before','less','autoprefixer','dom_munger','ngtemplates','cssmin','concat','merge-json','ngAnnotate','uglify','copy','htmlmin','clean:after']);
+  grunt.registerTask(
+     'build',
+     [
+       'check_config',
+       'check_plugins_config',
+       'jshint',
+       'clean:before',
+       'less',
+       'autoprefixer',
+       'dom_munger',
+       'ngtemplates',
+       'cssmin',
+       'concat',
+       'merge-json',
+       'ngAnnotate',
+       'uglify',
+       'copy',
+       'htmlmin',
+       'clean:after'
+     ]
+  );
   grunt.registerTask('serve', ['dom_munger:read','jshint','connect', 'watch']);
   grunt.registerTask('test',['dom_munger:read','karma:all_tests']);
 
