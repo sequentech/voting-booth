b5c9f0c8a0349d53b816a515902d5b6d5d0e95ae
Eduardo Robles Elvira <edulix@nvotes.com>
2018-10-15 09:55:28 +0200

    add initial i18n for fi

--- Gruntfile.js
+++ Gruntfile.js
@@ -255,7 +255,8 @@ module.exports = function (grunt) {
             "dist/locales/en.json": ["locales/en.json", "plugins/**/locales/en.json", "bower_components/avCommon/locales/en.json"],
             "dist/locales/es.json": ["locales/es.json", "plugins/**/locales/es.json", "bower_components/avCommon/locales/es.json"],
             "dist/locales/gl.json": ["locales/gl.json", "plugins/**/locales/gl.json", "bower_components/avCommon/locales/gl.json"],
-            "dist/locales/ca.json": ["locales/ca.json", "plugins/**/locales/ca.json", "bower_components/avCommon/locales/ca.json"]
+            "dist/locales/ca.json": ["locales/ca.json", "plugins/**/locales/ca.json", "bower_components/avCommon/locales/ca.json"],
+            "dist/locales/fi.json": ["locales/fi.json", "plugins/**/locales/fi.json", "bower_components/avCommon/locales/fi.json"]
         }
       }
     },
