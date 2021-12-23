7df05f82f6541ec24f2195ef41fffa854ed13318
Eduardo Robles Elvira <edu@sequentech.io>
2021-12-23 11:37:26 +0100

    initial work to unify all booth headers in a single file to remove duplication

---
+++ avBooth/booth-header-directive/booth-header-directive.js
@@ -0,0 +1,31 @@
+/**
+ * This file is part of agora-gui-booth.
+ * Copyright (C) 2015-2021 Sequent Tech Inc <legal@sequentech.io>
+
+ * agora-gui-booth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+
+ * agora-gui-booth  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with agora-gui-booth.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/*
+ * Directive that shows the booth header.
+ */
+angular.module('avBooth')
+  .directive('avbBoothHeader', function() {
+
+    var link = function(_scope, _element, _attrs) {
+    };
+    return {
+      restrict: 'AE',
+      link: link,
+      templateUrl: 'avBooth/booth-header-directive/booth-header-directive.html'
+    };
+  });
