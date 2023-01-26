6a855fa35b567760fbf1ad66d3fb198c3503ded7
Eduardo Robles Elvira <edu@sequentech.io>
2023-01-26 14:00:34 +0100

    adding french i18n

--- Gruntfile.js
+++ Gruntfile.js
@@ -276,6 +276,11 @@ module.exports = function (grunt) {
             "plugins/**/locales/es.json", 
             "node_modules/common-ui/dist/locales/es.json"
           ],
+          "dist/locales/fr.json": [
+            "locales/fr.json", 
+            "plugins/**/locales/fr.json", 
+            "node_modules/common-ui/dist/locales/fr.json"
+          ],
           "dist/locales/gl.json": [
             "locales/gl.json", 
             "plugins/**/locales/gl.json", 
@@ -328,6 +333,7 @@ module.exports = function (grunt) {
 
           "dist/locales/moment/en.js": "node_modules/moment/locale/en-gb.js",
           "dist/locales/moment/es.js": "node_modules/moment/locale/es.js",
+          "dist/locales/moment/fr.js": "node_modules/moment/locale/fr.js",
           "dist/locales/moment/gl.js": "node_modules/moment/locale/gl.js",
           "dist/locales/moment/ca.js": "node_modules/moment/locale/ca.js"
         }
