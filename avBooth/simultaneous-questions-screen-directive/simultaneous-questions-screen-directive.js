1eb8d94a89a92bbe58d9e98f1b0727cfb9ab3c8b
Findeton <felix@sequentech.io>
2023-01-30 19:10:51 +0100

    wip

--- avBooth/simultaneous-questions-screen-directive/simultaneous-questions-screen-directive.js
+++ avBooth/simultaneous-questions-screen-directive/simultaneous-questions-screen-directive.js
@@ -122,6 +122,19 @@ angular.module('avBooth')
                   },
                   postfix: "-blank"
                 },
+                // raise if vote is explicitly invalid if not checkerTypeFlag
+                {
+                  check: "lambda",
+                  validator: function (question)
+                  {
+                    return !(
+                      checkerTypeFlag === "warn-invalid-implicit-and-explicit" &&
+                      question.invalidVoteAnswer &&
+                      question.invalidVoteAnswer.selected > -1
+                    )
+                  },
+                  postfix: "-invalid"
+                },
                 // raise if numSelectedOptions < min, but not if blank, and
                 // checkerTypeFlag is normal and invalidVoteAnswer is not set
                 {
