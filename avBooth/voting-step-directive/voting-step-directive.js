2ce665e3b9740d4ae7d75f4cb9de4fa05db3c97f
Findeton <felix@sequentech.io>
2023-02-27 22:01:27 -0500

    wip

---
+++ avBooth/voting-step-directive/voting-step-directive.js
@@ -0,0 +1,35 @@
+/**
+ * This file is part of voting-booth.
+ * Copyright (C) 2023  Sequent Tech Inc <legal@sequentech.io>
+
+ * voting-booth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+
+ * voting-booth  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with voting-booth.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+angular
+  .module('avBooth')
+  .directive(
+    'avbVotingStep',
+    function(ConfigService)
+    {
+      function link(scope, element, attrs)
+      {
+      }
+
+      return {
+        restrict: 'AE',
+        scope: true,
+        link: link,
+        templateUrl: 'avBooth/voting-step-directive/voting-step-directive.html'
+      };
+    }
+  );
