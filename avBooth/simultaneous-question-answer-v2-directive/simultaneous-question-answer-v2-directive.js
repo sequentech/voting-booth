6fcce5e793e06d7da7ca777ea4b475a670ac6b24
Eduardo Robles <edu@sequentech.io>
2023-09-06 10:21:59 +0200

    ✨ Write-ins multiple fields (#350) (#386)
    Parent issue: https://github.com/sequentech/meta/issues/125
    
    Co-authored-by: Félix Robles <felix@sequentech.io>

--- avBooth/simultaneous-question-answer-v2-directive/simultaneous-question-answer-v2-directive.js
+++ avBooth/simultaneous-question-answer-v2-directive/simultaneous-question-answer-v2-directive.js
@@ -25,20 +25,69 @@ angular.module('avBooth')
       ErrorCheckerGeneratorService
     ) 
     {
+      // used to interpolate write-in field values into its templated string
+      function interpolateWriteIn(template, fields) {
+        var interpolatedText = template;
+        Object.values(fields).map(function (field) {
+          var regex = new RegExp(`{${field.id}}`, "g");
+          interpolatedText = interpolatedText.replace(regex, _.isString(field.value)? field.value : "");
+        });
+
+        var allEmptyFields = Object.values(fields).every(function (field) { return !field.value; });
+        if (allEmptyFields) {
+          return "";
+        }
+
+        return interpolatedText;
+      }
       function link(scope, _element, _attrs)
       {
         scope.isCategoryList = ErrorCheckerGeneratorService.hasUrl(scope.answer.urls, 'isCategoryList', 'true');
         scope.isWriteIn = ErrorCheckerGeneratorService.hasUrl(scope.answer.urls, 'isWriteIn', 'true');
+        scope.withWriteInConfig = _.isObject(scope.question.extra_options) &&
+          _.isObject(scope.question.extra_options.write_in_config);
 
         if (scope.isWriteIn && scope.writeInTextChange) 
         {
           scope.$watch(
             "answer.text",
-            function () 
+            function ()
             {
               scope.writeInTextChange();
             }
           );
+
+          // manage write-in fields
+          if (scope.withWriteInConfig) {
+            // create a copy of the fields, with empty values
+            var writeInFields = scope.question.extra_options.write_in_config.fields.map(
+              _.clone
+            );
+            // if it doesn't exist, initialize
+            // don't change it if the variable exists as this means editing after review
+            // and it would wipe out current values
+            if (_.isUndefined(scope.answer.writeInFields)) {
+              scope.answer.writeInFields = _.object(
+                _.pluck(writeInFields, "id"),
+                writeInFields
+              );
+            }
+            var template = scope.question.extra_options.write_in_config.template;
+
+            // watch changes for the write-in field values to update the templated text
+            writeInFields.map(function (field) {
+              scope.$watch(
+                `answer.writeInFields.${field.id}.value`,
+                function (newValue,_oldValue)
+                {
+                  if (_.isUndefined(newValue)) {
+                    return;
+                  }
+                  scope.answer.text = interpolateWriteIn(template, scope.answer.writeInFields);
+                }
+              );
+            });
+          }
         }
 
         scope.isCheckSelected = function(answer, check)
